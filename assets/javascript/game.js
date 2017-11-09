5d51d03c86a1006e50f028eba7699636577b2cc6
Travis Roberts <travis.roberts2@gmail.com>
2017-11-09 11:24:39 -0600

    jquery logic and hangman images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,76 @@
 var instruments = ["mandolin", "harpsichord", "electric bass", "bagpipes", "electric guitar", "xylophone"];
+var blanks = [];
+var lives = 7;
+var guesses = [];
+var wordsGuessed = [];
+var randomWord = "";
+var computerGuess = instruments[Math.floor(Math.random() * instruments.length)];
+var word = computerGuess.split("");
 
-// Randomly chooses from the instruments array. This is the Computer's guess.
-function randomGuess() {
-  var computerGuess = instruments[Math.floor(Math.random() * instruments.length)];
-  return(computerGuess);
+  // Randomly chooses from the instruments array. This is the Computer's guess.
+function startGame() {
+  console.log(word);
+  $("#hangman").html("<img src=./assets/images/Hangman-0.png>");
+  function blankGen() {
+    for (i = 0;i < word.length;i++) {
+      blanks[i] = "_";
+      console.log(blanks);
+      $("#click").html("Current Word: " + blanks.join(' '));
+      $("#wordsGuessed").html("Words Guessed: "+ wordsGuessed.join(" "));
+      $("#lives").html("Guesses Remaining: " + lives);
+    }
+    for(i = 0;i < wordsGuessed.length;i++) {
+      $("#hangman").html("<img src=./assets/images/Hangman-" + [i + 1] + ".png>");
+    }
   }
+  blankGen();
+    }
+    document.onkeyup = function(press){
+        var guess = (press.key);
+        console.log(guess);
+        guesses.push(guess);
+        for(i = 0; i < guesses.length; i++)
+          for(j = 0; j<blanks.length; j++){
+            if(guess == word[j]){
+              console.log("correct!");
+              blanks[j] = guess;
+            }
+          }
+        if(word.indexOf(guess) < 0){
+            lives -- ;
+            wordsGuessed.push(guess);
+        }
 
-var a = randomGuess();
-var b = a.split("");
-console.log(b);
+        for(i = 0; i<wordsGuessed.length ; i++){
+            $("#hangman").html("<img src=\"assets/images/Hangman-" + [i + 1] +".png\">");
+            if(i > 5){
+              $("#hangman").html("<h2>YOU LOSE!")
+            }
+        }
+        $("#click").html("Current Word: " + blanks.join(' '));
+        $("#lives").html("Guesses Remaining: " + lives);
+        $("#wordsGuessed").html("Words Guessed: " + "<br>" + wrongGuesses.join(" "));
+        // $(".jumbotron").css("margin-bottom", "417px");
 
-  function startGame() {
-      $("#click").text(b);
-  }
+    }
 
-    console.log(b);
+      // $("newGame").on('click', function() {
+      //   blanks = [];
+      //   lives = 7;
+      //   guesses = [];
+      //   wrongGuesses = [];
+      //   randomWord = "";
 
 
-    var numbers = [1, 5, 10, 15];
-    var doubles = numbers.map(function test(y) {
-       return y * 3;
-    });
 
-    console.log(doubles);
+
+
+    // var numbers = [1, 5, 10, 15];
+    // var doubles = numbers.map(function test(y) {
+    //    return y * 3;
+    // });
+    //
+    // console.log(doubles);
 
 
 
