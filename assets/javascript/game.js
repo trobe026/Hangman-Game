cd9296465d1521c9de07b4018b92ed0453394b79
Travis Roberts <travis.roberts2@gmail.com>
2017-11-10 00:27:44 -0600

    more jquery logic, background images and styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // start game variables
-var instruments = ["mandolin", "harpsichord", "electric bass", "bagpipes", "electric guitar", "xylophone"];
+var instruments = ["mandolin", "harpsichord", "electricbass", "bagpipes", "electricguitar", "xylophone", "ukulele", "tambourine", "theremin", "synthesizer", "clarinet", "saxophone", "flugelhorn", "glockenspiel", "recorder"];
 var blanks = [];
 var lives = 7;
 // onkey variabes
@@ -7,18 +7,38 @@ var guesses = [];
 var lettersGuessed = [];
 var computerGuess = instruments[Math.floor(Math.random() * instruments.length)];
 var word = computerGuess.split("");
+$("#newGame").css("visibility", "hidden");
 
   // Randomly chooses from the instruments array. This is the Computer's guess.
 function startGame() {
+  computerGuess = instruments[Math.floor(Math.random() * instruments.length)];
+  word = computerGuess.split("");
   console.log(word);
   $("#hangman").html("<img src=./assets/images/Hangman-0.png>");
+  $("body").css({
+    "font-family": "'Passion One', cursive",
+    "font-family": "'Josefin Slab', serif",
+    "background": "url('./assets/images/intersection.png')"
+  });
+
+
   function blankGen() {
     for (i = 0;i < word.length;i++) {
       blanks[i] = "_";
       console.log(blanks);
-      $("#currentWord").html("Current Word: " + blanks.join(" "));
-      $("#lettersGuessed").html("Words Guessed: "+ lettersGuessed.join(" "));
-      $("#lives").html("Guesses Remaining: " + lives);
+      $("#currentWord").html("Current Word: " + "<br>" + blanks.join(" ")).css({
+        margin: "10px",
+        "padding-top": "20px",
+        width: "100%"
+      });
+      $("#lettersGuessed").html("Words Guessed: "+ lettersGuessed.join(" ")).css({
+        margin: "10px",
+        "padding-top": "20px"
+      });;
+      $("#lives").html("Guesses Remaining: " + lives).css({
+        margin: "10px",
+        "padding-top": "20px"
+      });
     }
     for(i = 0;i < lettersGuessed.length;i++) {
       $("#hangman").html("<img src=./assets/images/Hangman-" + [i + 1] + ".png>");
@@ -26,6 +46,8 @@ function startGame() {
   }
   blankGen();
     }
+
+
     document.onkeyup = function(press){
         var guess = (press.key);
         console.log(guess);
@@ -35,23 +57,28 @@ function startGame() {
             if(guess == word[j]){
               console.log("correct!");
               blanks[j] = guess;
+              if(guesses == blanks) {
+                $("#hangman").html("<h2>YOU WIN!!!</h2>")
+              }
             }
           }
         if(word.indexOf(guess) < 0){
             lives -- ;
             lettersGuessed.push(guess);
         }
-        if(guesses == word) {
-          $("#hangman").html("<h2>YOU WIN!!!</h2>")
-        }
 
         for(i = 0; i<lettersGuessed.length ; i++){
             $("#hangman").html("<img src=assets/images/Hangman-" + [i + 1] +".png>");
             if(i > 5){
-              $("#hangman").html("<h2>YOU LOSE!</h2>")
+              $('#newGame').remove();
+              $("#hangman").html("<h1>YOU LOSE!</h1>").css({
+                color: "blue",
+                width: "300px",
+                "background-color": "red"
+              });
             }
         }
-        $("#currentWord").html("Current Word: " + blanks.join(' '));
+        $("#currentWord").html("Current Word: " + "<br>" + blanks.join(' '));
         $("#lives").html("Guesses Remaining: " + lives);
         $("#lettersGuessed").html("Letters Guessed: " + "<br>" + lettersGuessed.join(" "));
         $("#col-sm-12").css("margin-bottom", "417px");
@@ -63,7 +90,7 @@ function startGame() {
       lives = 7;
       guesses = [];
       lettersGuessed = [];
-
+      startGame();
     }
 
       // $("newGame").on('currentWord', function() {
